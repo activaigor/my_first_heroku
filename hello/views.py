a67ad4dce0c0ec4fe60974681c6835ac308a0e91
activaigor <activa commit>
2014-09-11 16:12:30 +0300

    Demo

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 from .models import Greeting
 
@@ -7,6 +8,11 @@ from .models import Greeting
 def index(request):
     return HttpResponse('Hello from Python!')
 
+
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print r.text
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 def db(request):
 
